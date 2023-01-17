07a3cda0e25347bbf60e510a7d6b99b96064ab47
Marcelo Suarez <amsuarez99@gmail.com>
2023-01-17 15:46:37 -0600

    add common configs folder

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,9 +1,3 @@
-import { Options } from "tsup";
-const config: Options = {
-  clean: true,
-  format: "esm",
-  sourcemap: true,
-  entry: ["src/index.ts"],
-};
+import config from "../../configs/tsup.config.js";
 
 export default config;
